5a235faa8bf6e8a0443c246083521dd6cdeca4b8
Baron <btniu@andrew.cmu.edu>
2025-04-30 00:05:20 -0400

    Connecting to GameData totalcoins through ShipUnlockerChecker.java

--- src/main/java/com/dinosaur/dinosaurexploder/utils/ShipUnlockChecker.java
+++ src/main/java/com/dinosaur/dinosaurexploder/utils/ShipUnlockChecker.java
@@ -2,6 +2,7 @@ package com.dinosaur.dinosaurexploder.utils;
 
 import com.dinosaur.dinosaurexploder.exception.LockedShipException;
 import com.dinosaur.dinosaurexploder.model.HighScore;
+import com.dinosaur.dinosaurexploder.model.TotalCoins;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -22,6 +23,7 @@ public class ShipUnlockChecker {
             8, 700
     );
     private HighScore highScore = new HighScore();
+    private TotalCoins totalCoins = new TotalCoins(); 
 
     public int check(int shipNumber) {
         highScore = getHighScore();
@@ -44,4 +46,13 @@ public class ShipUnlockChecker {
         throw new LockedShipException(languageManager.getTranslation("ship_locked") + "\n" +
                languageManager.getTranslation("unlock_highScore").replace("##", String.valueOf(lowerLimit)));
     }
+
+    public TotalCoins getTotalCoins() {
+        try (FileInputStream file = new FileInputStream("totalCoins.ser");
+            ObjectInputStream in = new ObjectInputStream(file)) {
+            return (TotalCoins) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            return new TotalCoins(); 
+        }
+     }
 }

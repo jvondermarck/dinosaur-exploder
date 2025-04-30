b3d1ef2054f1ceb0cdcd450e58ac21b3306d0798
Baron <btniu@andrew.cmu.edu>
2025-04-29 23:53:31 -0400

    Modified CollectedCoinsComponent to increment total coins

--- src/main/java/com/dinosaur/dinosaurexploder/components/CollectedCoinsComponent.java
+++ src/main/java/com/dinosaur/dinosaurexploder/components/CollectedCoinsComponent.java
@@ -16,6 +16,7 @@ public class CollectedCoinsComponent extends Component implements CollectedCoins
     private int coin = 0;
     private final int COIN_VALUE = 1;
 
+    private static TotalCoins totalCoins = new TotalCoins();
 
     private final LanguageManager languageManager = LanguageManager.getInstance();
 
@@ -24,6 +25,8 @@ public class CollectedCoinsComponent extends Component implements CollectedCoins
 
     @Override
     public void onAdded() {
+        loadCoins(); // Deserialize once when the component is added
+
         // Create UI elements
         coinText = new Text();
         coinText.setFill(Color.GOLDENROD);
@@ -47,13 +50,38 @@ public class CollectedCoinsComponent extends Component implements CollectedCoins
         return container;
     }
 
+    private void loadCoins() {
+        try (FileInputStream file = new FileInputStream("totalCoins.ser");
+            ObjectInputStream in = new ObjectInputStream(file)) {
+            totalCoins = (TotalCoins) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            totalCoins = new TotalCoins();
+        }
+    }
+
+    private void saveCoins() {
+        try (FileOutputStream file = new FileOutputStream("totalCoins.ser");
+            ObjectOutputStream out = new ObjectOutputStream(file)) {
+            out.writeObject(totalCoins);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static TotalCoins getTotalCoins() {
+        return totalCoins;
+    }
+
     @Override
     public void onUpdate(double tpf) {
         updateText();
     }
 
-    public void incrementCoin(){
-        coin += COIN_VALUE;
+    public void incrementCoin() {
+        coin += 1;
+        totalCoins.setTotal(totalCoins.getTotal() + 1);
         updateText();
+        saveCoins();
+        // System.out.println("Total: " + totalCoins.getTotal());
     }
 }

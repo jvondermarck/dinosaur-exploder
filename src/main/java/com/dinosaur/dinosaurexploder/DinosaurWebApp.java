1c7cc646e3fb043f6c4cc6cb9bae0af55354fbd1
jvondermarck <vdmjulien05@gmail.com>
2025-12-13 22:57:01 +0100

    chore: ensure JPro web mode is enabled and follow best practices in start method

--- src/main/java/com/dinosaur/dinosaurexploder/DinosaurWebApp.java
+++ src/main/java/com/dinosaur/dinosaurexploder/DinosaurWebApp.java
@@ -11,13 +11,12 @@ public class DinosaurWebApp extends JProApplication {
   @Override
   public void start(Stage stage) {
     System.setProperty("fxgl. isBrowser", "true");
-    System.setProperty("jpro.web.mode", "true");  // <--- AJOUTEZ CETTE LIGNE pour plus de sûreté
+    System.setProperty("jpro.web.mode", "true");
 
     GameApplication app = new DinosaurApp();
     var fxglRoot = GameApplication.embeddedLaunch(app);
 
     stage.setScene(new Scene(new StackPane(fxglRoot)));
-    stage.show();  // <--- AJOUTEZ AUSSI CETTE LIGNE (bonne pratique)
   }
 
   public static void main(String[] args) {

97a4a3da03f2da2bfe4bbaf1ca50120044218afc
Julien Von Der Marck <62793491+jvondermarck@users.noreply.github.com>
2025-12-17 22:57:49 +0100

    Update EntityType.java

--- src/main/java/com/dinosaur/dinosaurexploder/constants/EntityType.java
+++ src/main/java/com/dinosaur/dinosaurexploder/constants/EntityType.java
@@ -1,12 +1,20 @@
 package com.dinosaur.dinosaurexploder.constants;
 
-/** This handles with all the entities in the game like lives, player projectile etc. */
+/** Thi
+s handles with all the entities in the game like lives, player projectile etc. */
 public enum EntityType {
+
+  
   PLAYER,
 
   GREEN_DINO,
   RED_DINO,
   ORANGE_DINO,
+
+
+
+
+  
   PROJECTILE,
   ENEMY_PROJECTILE,
   SCORE,

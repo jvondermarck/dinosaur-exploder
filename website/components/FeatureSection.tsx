c2b23de7b08e89c2bff20f002f1786d38ae028e3
melinagiovanoudi <meligiov@csd.auth.gr>
2026-01-17 22:34:04 +0200

    added multilingual text on the website

--- website/components/FeatureSection.tsx
+++ website/components/FeatureSection.tsx
@@ -3,25 +3,10 @@ type Feature = {
   title: string;
 };
 
-const features: Feature[] = [
-  {
-    icon: "🦖",
-    title: "Pixel Dino Action",
-  },
-  {
-    icon: "🕹️",
-    title: "Classic Gameplay",
-  },
-  {
-    icon: "🌍",
-    title: "Open source & Free",
-  },
-];
-
-export default function FeatureSection() {
+export default function FeatureSection({dict}: {dict: Feature[] }) {
   return (
     <section className="max-w-4xl mx-auto w-full py-8 px-4 grid grid-cols-1 md:grid-cols-3 gap-4">
-      {features.map((feature, idx) => (
+      {dict.map((feature, idx) => (
         <div
           key={idx}
           className="bg-black/80 border-2 border-green-700 rounded-lg p-6 text-center text-green-100"

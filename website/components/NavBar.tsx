404f37133f60df0ef981f8f2a01e32b60e75785b
Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
2026-01-23 10:02:36 -0500

    feat: mobile menu addition
    Signed-off-by: Egbaiyelo <moteniolaegbaiyelo@trentu.ca>

--- website/components/NavBar.tsx
+++ website/components/NavBar.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { usePathname, useParams } from "next/navigation";
 import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { MdClose } from "react-icons/md";
 
 export default function NavBar({ lang, dict }: { lang: string; dict: any }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,10 +30,10 @@ export default function NavBar({ lang, dict }: { lang: string; dict: any }) {
         </Link>
 
         {/* Navigation */}
-        <div className="font-retro flex flex-col sm:flex-row gap-2 sm:items-center justify-between ">
+        <div className={`font-retro flex flex-col sm:flex-row gap-2 sm:items-center justify-between `}>
 
           {/* Internal links */}
-          <div className="flex xs:flex-row gap-2 justify-center">
+          <div className={`hidden lg:flex  xs:flex-row gap-2 justify-center`}>
             {navItems.map((item) => {
               const localisedHref = `/${lang}${item.href}`;
               const isActive = pathname === localisedHref;
@@ -54,7 +56,7 @@ export default function NavBar({ lang, dict }: { lang: string; dict: any }) {
           </div>
 
           {/* External links */}
-          <div className="flex xs:flex-row gap-2 justify-center">
+          <div className={`hidden xl:flex  xs:flex-row gap-2 justify-center`}>
             <a
               href="https://github.com/jvondermarck/dinosaur-exploder"
               target="_blank"
@@ -87,7 +89,91 @@ export default function NavBar({ lang, dict }: { lang: string; dict: any }) {
               href="https://github.com/sponsors/jvondermarck"
               target="_blank"
               rel="noopener noreferrer"
-              className={`${buttonStyle} shadow-[4px_4px_0px_0px_#A3245C] border-pink-700 text-white bg-pink-600 hover:bg-pink-700 justify-center`}
+              className={`${buttonStyle} shadow-[3px_3px_0px_0px_#A3245C] border-pink-700 text-white bg-pink-600 hover:bg-pink-700 justify-center`}
+            >
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path d="M8 15C-7.333 4.868 3.278-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.723-3.042 23.333 4.868 8 15z" />
+              </svg>
+              Sponsor
+            </a>
+          </div>
+
+          {/* Small Screen / Mobile Toggle Button */}
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className=" xl:hidden p-2 border-2 border-green-800 rounded shadow-[2px_2px_0px_0px_theme(colors.green.700)] active:shadow-none active:translate-x-[2px] active:translate-y-[2px] text-green-800 cursor-pointer"
+          >
+            {isOpen ? <MdClose size={20} /> : <RxHamburgerMenu size={20} />}
+          </button>
+        </div>
+
+      </div>
+
+
+      {/* +++++ Hidden menu items +++++ */}
+      
+      {isOpen && (
+        <div className="mx-auto md:absolute md:right-6 sm:right-6 z-50 md:ml-auto xl:hidden w-fit flex flex-col gap-4 mt-4 p-4 md:border-2 md:border-green-800 md:bg-white md:shadow-[3px_3px_0px_0px_theme(colors.green.700)] rounded">
+
+          {/* Internal links */}
+          <div className="lg:hidden flex flex-col sm:flex-row md:flex-col  gap-2">
+            {navItems.map((item) => (
+              <Link
+                key={item.href}
+                href={`/${lang}${item.href}`}
+                onClick={() => setIsOpen(false)}
+                className={`${buttonStyle} border-green-700 text-green-800 bg-white justify-center shadow-[2px_2px_0px_0px_theme(colors.green.700)]`}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </div>
+
+          <hr className="sm:hidden border-green-800/20 w-full" />
+
+          {/* External Links */}
+          <div className="flex flex-col sm:flex-row md:flex-col gap-2">
+            <a
+              href="https://github.com/jvondermarck/dinosaur-exploder"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`${buttonStyle} justify-center shadow-[3px_3px_0px_0px_theme(colors.green.700)] border-green-800 text-green-800 bg-white hover:bg-green-800 hover:text-white hover:border-green-800`}
+            >
+              <FaGithub size={25} />
+              Github
+            </a>
+
+
+            <a
+              href="https://github.com/jvondermarck/dinosaur-exploder/stargazers"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`${buttonStyle} shadow-[3px_3px_0px_0px_theme(colors.yellow.400)] border-yellow-500 text-yellow-700 bg-white hover:bg-yellow-500 hover:text-white justify-center `}
+            >
+              <svg
+                width="22"
+                height="22"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z" />
+              </svg>
+              Star
+            </a>
+
+
+            <a
+              href="https://github.com/sponsors/jvondermarck"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`${buttonStyle} shadow-[3px_3px_0px_0px_#A3245C] border-pink-700 text-white bg-pink-600 hover:bg-pink-700 justify-center`}
             >
               <svg
                 width="18"
@@ -102,7 +188,7 @@ export default function NavBar({ lang, dict }: { lang: string; dict: any }) {
             </a>
           </div>
         </div>
-      </div>
+      )}
     </nav>
   );
 }

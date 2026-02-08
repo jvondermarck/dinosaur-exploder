06f4fcf0be31d154b7f6e3165eb4cc5285ed9369
Karthik Gudaru <karthikgudaru72@gmail.com>
2026-02-08 20:28:53 +0530

    Update src/main/java/com/dinosaur/dinosaurexploder/controller/CoinSpawner.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/com/dinosaur/dinosaurexploder/controller/CoinSpawner.java
+++ src/main/java/com/dinosaur/dinosaurexploder/controller/CoinSpawner.java
@@ -26,8 +26,8 @@ public class CoinSpawner {
         run(
             () -> {
               if (random(0, 100) < percentChanceForCoinDrop) {
-                  double x = random(0, getAppWidth() - 80);
-                  spawn("coin", x, 0);
+                double x = random(0, getAppWidth() - 80);
+                spawn("coin", x, 0);
               }
             },
             seconds(duration));

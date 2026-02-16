3e6d875821cab0ac494558e315b9cf80c697b28d
Daniel Romero <romero.daniel@ufl.edu>
2026-02-16 14:31:03 -0500

    Apply maven formatting spotless

--- src/main/java/com/dinosaur/dinosaurexploder/achievements/AchievementPersistence.java
+++ src/main/java/com/dinosaur/dinosaurexploder/achievements/AchievementPersistence.java
@@ -7,31 +7,32 @@ import java.util.Properties;
 
 public class AchievementPersistence {
 
-    private static final String FILE_NAME = "achievements.properties";
+  private static final String FILE_NAME = "achievements.properties";
 
-    public static void save(String achievementId) {
-        Properties props = new Properties();
-        try (FileInputStream in = new FileInputStream(FILE_NAME)) {
-            props.load(in);
-        } catch (IOException ignored) {}
-
-        props.setProperty(achievementId, "true");
-
-        try (FileOutputStream out = new FileOutputStream(FILE_NAME)) {
-            props.store(out, "Unlocked Achievements");
-            System.out.println("Saved achievement: " + achievementId);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  public static void save(String achievementId) {
+    Properties props = new Properties();
+    try (FileInputStream in = new FileInputStream(FILE_NAME)) {
+      props.load(in);
+    } catch (IOException ignored) {
     }
 
-    public static boolean isUnlocked(String achievementId) {
-        Properties props = new Properties();
-        try (FileInputStream in = new FileInputStream(FILE_NAME)) {
-            props.load(in);
-            return "true".equals(props.getProperty(achievementId));
-        } catch (IOException e) {
-            return false;
-        }
+    props.setProperty(achievementId, "true");
+
+    try (FileOutputStream out = new FileOutputStream(FILE_NAME)) {
+      props.store(out, "Unlocked Achievements");
+      System.out.println("Saved achievement: " + achievementId);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
+
+  public static boolean isUnlocked(String achievementId) {
+    Properties props = new Properties();
+    try (FileInputStream in = new FileInputStream(FILE_NAME)) {
+      props.load(in);
+      return "true".equals(props.getProperty(achievementId));
+    } catch (IOException e) {
+      return false;
+    }
+  }
 }
